162548fa3c2ffe9a7a79b49aaff624e2bd4b26c9
adamcope <adam@conquestofthread.com>
2022-04-30 00:45:50 -0500

    lesson 21

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import "./App.css";
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-  const  [name, setName] = useState('mario')
+  const [events, setEvents] = useState([
+    { title: "mario's birthday bash", id: 1 },
+    { title: "bowser's live stream", id: 2 },
+    { title: "race on moo moo farm", id: 3 },
+  ]);
 
-  const handleClick = () => {
-    setName('luigi')
-    console.log(name);
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id
+      })
+    })
+    console.log(id);
   };
+
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change Name</button>
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>
+            {" "}
+            {index + 1} - {event.title}{" "}
+          </h2>
+          <button onClick={() => handleClick(event.id)}>Delete Event</button>
+        </div>
+      ))}
     </div>
   );
 }
